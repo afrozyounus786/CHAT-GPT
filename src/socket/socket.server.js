cf491104905d5a116823660d23177c0ffc3ee4b0
afrozyounus786 <afrozyounus26@gmail.com>
2025-10-21 16:19:05 +0530

    Implemented Gemini API and Short Term Memory

--- src/socket/socket.server.js
+++ src/socket/socket.server.js
@@ -1,38 +1,72 @@
 const { Server } = require("socket.io");
 const cookie = require("cookie");
 const jwt = require("jsonwebtoken");
-const User  = require("../model/user.model");
+const User = require("../model/user.model");
+const { generateResponse } = require("../service/ai.service");
+const Message = require("../model/message.model");
 
 function initSocketServer(httpServer) {
-  const io = new Server(httpServer,{});
+  const io = new Server(httpServer, {});
 
   // Middleware to parse cookies from the handshake headers
   // For Authentication process of the socket connection
-  io.use(async (socket, next)=>{
-    const cookies = cookie.parse(socket.handshake.headers?.cookie || ""); 
+  io.use(async (socket, next) => {
+    const cookies = cookie.parse(socket.handshake.headers?.cookie || "");
 
-    if(!cookies.token){
+    if (!cookies.token) {
       return next(new Error("Authentication error: No token provided"));
     }
 
     try {
-      
       const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
 
       const user = await User.findById(decoded.id);
       socket.user = user;
 
       next();
-
     } catch (error) {
       return next(new Error("Authentication error: Invalid token"));
     }
-  })
+  });
 
   io.on("connection", (socket) => {
-    console.log("User Connected:", socket.user);
-    console.log("New client connected:", socket.id);
+    //The message send by the AI will be received here
+    socket.on("ai-message", async (data) => {
+
+      await Message.create({
+        user: socket.user._id,
+        chat: data.chat,
+        content: data.content,
+        role: "user",
+      })
+
+      const chatHistory = await Message.find({
+        chat: data.chat,
+      })
+
+      //Short Term memory implementation
+      const response = await generateResponse(chatHistory.map((item)=>{
+        return {
+          role: item.role,
+          parts: [{
+            text: item.content,
+          }]
+        }
+      }));
+
+      await Message.create({
+        user: socket.user._id,
+        chat: data.chat,
+        content: response,
+        role: "model",
+      })
+
+      socket.emit("ai-response", {
+        content: response,
+        chat: data.chat,
+      });
+    });
   });
 }
 
-module.exports = initSocketServer
+module.exports = initSocketServer;
